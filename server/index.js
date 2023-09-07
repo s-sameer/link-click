b89d3cf64cb839649801a241d1e14148c932d5fa
Sadman Sameer <itsnotsameer@gmail.com>
2023-09-07 01:23:53 -0230

    initial commit

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import multer from 'multer';
+import helmet from helmet;
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+//Middlewares in express are functions that can process or manipulate incoming HTTP requests or outgoing responses before they reach their final destination
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy : "cross-origin"}))
+app.use(morgan('common'))
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")))
+
+//File storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/assets")
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({storage: storage})
